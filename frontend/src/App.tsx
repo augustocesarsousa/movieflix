047898d09f20683fdcc06f71f98d4da09c86bd2a
augusto cesar <acs-03@hotmail.com>
2021-12-10 22:09:41 -0300

    Frontend started

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './assets/styles/custom.scss';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MovieFlix</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
